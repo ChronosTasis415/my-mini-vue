7faa09c945054fed4080b1f40cfd280f7e25197f
zhangleipeng <547493397@qq.com>
2022-07-14 20:25:31 +0800

    feat(effect): effect返回runner功能；effect的scheduler调度功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -2,24 +2,32 @@ let activeEffect;
 // 将传入的fn 进行包装 变成类的私有变量 外部访问不到(只能在类中访问)
 class ReactiveEffect {
   private _fn;
-  constructor(fn) {
+  constructor(fn, public scheduler?) {
     this._fn = fn;
   }
 
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 
 
 
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn);
+export function effect(fn, options:any = {}) {
+  const {scheduler } = options;
+  const _effect = new ReactiveEffect(fn, scheduler);
+
+  // 首次执行的时候 只执行runner 不管有没有scheduler
   _effect.run();
+  
+  // bind 绑定实例对象 
+  return _effect.run.bind(_effect);
 }
 
 const targetMap = new Map();
+
+// 收集依赖
 export function track(target, key) {
 
   // 需要收集的fn不可重复 所以用Set
@@ -47,11 +55,16 @@ export function track(target, key) {
   //dep [ReactiveEffect]
 }
 
+// 触发依赖
 export function trigger(target, key) {
   const depsMap = targetMap.get(target);
   const deps = depsMap.get(key);
 
   for (const dep of deps) {
-    dep.run();
+    if (dep.scheduler) {
+      dep.scheduler();
+    } else {
+      dep.run();
+    }
   }
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -25,4 +25,66 @@ describe('effect', () => {
 
     expect(nextAge).toBe(14);
   })
+
+  it('effect runner', () => {
+    // 通过effect 第二个参数给的的一个scheduler的 fn
+    // effect 第一次执行的时候 执行fn 不执行scheduler
+    // 发生set 不执行fn 执行scheduler
+    // 执行runner 执行fn
+    let foo = 10;
+
+    const runner = effect(() => { 
+       foo++;
+
+       return 'foo';
+    })
+
+    expect(foo).toBe(11);
+
+    // effect() return 出来一个函数 即runner
+    // runner执行 return出来一个结果 即 r
+    const r = runner();
+    expect(foo).toBe(12);
+
+    expect(r).toBe('foo');
+  });
+
+
+  it('scheduler', () => {
+    let dummy;
+    let run: any;
+    let runner: any;
+
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+
+    const obj = reactive({foo:1});
+
+    runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+
+    // 起初 scheduler不会掉用
+    expect(scheduler).not.toHaveBeenCalled();
+
+    // 执行effect中的fn
+    expect(dummy ).toBe(1);
+    
+
+    obj.foo++;
+    
+    // 执行的是scheduler 而不是effect中的fn
+    expect(scheduler).toBeCalledTimes(1);
+
+    expect(dummy).toBe(1);
+
+    // effect的返回
+    run();
+    // 执行了 effect中的fn
+    expect(dummy ).toBe(2);
+  });
 })
