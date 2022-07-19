d8d968a3c6e4e18672fe65ca00fc4458051ee643
zhangleipeng <547493397@qq.com>
2022-07-19 15:36:22 +0800

    feat: 实现reactive和readonly嵌套功能

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -28,4 +28,16 @@ describe("readonly", () => {
     user.age++;
     expect(console.warn).toBeCalled();
   });
+
+  it("使嵌套的值为readonly", () => {
+    const original = { foo: 1, bar: { baz: 2 } };
+    const wrapped = readonly(original);
+
+    expect(wrapped).not.toBe(original);
+    expect(isReadonly(wrapped)).toBe(true);
+    expect(isReadonly(original)).toBe(false);
+    expect(isReadonly(wrapped.bar)).toBe(true);
+    expect(isReadonly(original.bar)).toBe(false);
+    expect(wrapped.foo).toBe(1);
+  });
 });
