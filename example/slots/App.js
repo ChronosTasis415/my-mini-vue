25eae4836f7182b89099ceb17b3d8e01f5c0f78b
zhangleipeng <547493397@qq.com>
2022-07-24 15:21:27 +0800

    feat(runtime-core): 实现Fragment和text节点功能

--- example/slots/App.js
+++ example/slots/App.js
@@ -1,4 +1,4 @@
-import { h } from "../../lib/guide-mini-vue.es.js";
+import { h, createTextVnode } from "../../lib/guide-mini-vue.es.js";
 import { Foo } from "./Foo.js";
 export const App = {
   render() {
@@ -8,7 +8,14 @@ export const App = {
       {},
       // 定义的是slots
       {
-        header: ({ age }) => h("p", {}, "header" + age),
+        header: ({ age }) => [
+          h("p", {}, "header" + age),
+          createTextVnode("你好你好你好"),
+        ],
+        // header: ({ age }) => [
+        //   h("p", {}, "header" + age),
+        //   h("section", {}, "App1111"),
+        // ],
         footer: () => h("p", {}, "footer"),
       }
     );
