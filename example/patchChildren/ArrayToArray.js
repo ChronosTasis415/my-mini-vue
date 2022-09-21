4189c1f8234d7406154abee3429180e462758be4
zhangleipeng <547493397@qq.com>
2022-09-21 10:50:10 +0800

    feat(element更新流程): 简单有序diff的实现

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -3,17 +3,81 @@ import { ref, h } from "../../lib/guide-mini-vue.es.js";
 // 1,左侧对比
 // (a b) c
 // (a b) de
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "E" }, "E"),
+// ];
+
+// 2,右侧对比
+// a (b c)
+// d e (b c)
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
+// const nextChildren = [
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
+
+// 3, 新的比老的长 也就是创建新的
+// 左侧
+// (a b)
+// (a b) c
+// i=2 e1=1 e2=2
+// const prevChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "D" }, "D"),
+// ];
+
+// 右侧
+// (a b)
+// c (a b)
+// i=0 e1=-1 e2=0
+// const prevChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
+// const nextChildren = [
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+// ];
+
+//4 老的比新的长
+//  删除老的
+// 左侧
+// (a b) c
+// (a b)
+// i=2 e1=2 e2=1
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
+// const nextChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
+
+// 右侧
+// c (a b)
+// (a b)
+// i=2 e1=2 e2=1
 const prevChildren = [
-  h("p", { key: "A" }, "A"),
-  h("p", { key: "B" }, "B"),
   h("p", { key: "C" }, "C"),
-];
-const nextChildren = [
   h("p", { key: "A" }, "A"),
   h("p", { key: "B" }, "B"),
-  h("p", { key: "D" }, "D"),
-  h("p", { key: "E" }, "E"),
 ];
+const nextChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
 
 export default {
   name: "ArrayToText",
--- src/runtime-dom/index.ts
+++ src/runtime-dom/index.ts
@@ -18,8 +18,8 @@ function patchProps(el, key, prevProp, nextProp) {
   }
 }
 
-function insert(el, container) {
-  container.append(el);
+function insert(child, parent, anchor) {
+  parent.insertBefore(child, anchor || null);
 }
 
 function setElementText(el, text) {
